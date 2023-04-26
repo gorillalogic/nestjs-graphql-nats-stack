3f3821ff3183b00bc3259933db9edf0e76310332
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-04-26 03:34:31 -0600

    Add CORS and graphql host

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,11 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_LOGIN_ORIGIN: string?;
-  VITE_LOGIN_RESPONSE_TYPE: string?;
-  VITE_LOGIN_SCOPE: string?;
-  VITE_LOGIN_CLIENT_ID: string?;
-  VITE_LOGIN_CODE_CHALLENGE_METHOD: string?;
+  VITE_LOGIN_ORIGIN: string;
+  VITE_LOGIN_RESPONSE_TYPE: string;
+  VITE_LOGIN_SCOPE: string;
+  VITE_LOGIN_CLIENT_ID: string;
+  VITE_LOGIN_CODE_CHALLENGE_METHOD: string;
+  VITE_GRAPHQL_HOST: string;
 }
 
 interface ImportMeta {
--- frontend/src/lib/graphql/client.ts
+++ frontend/src/lib/graphql/client.ts
@@ -2,5 +2,5 @@ import ApolloClient from "apollo-boost";
 
 // the Apollo cache is set up automatically
 export default new ApolloClient({
-  uri: 'http://3.88.235.94:3000/graphql'
+  uri: import.meta.env.VITE_GRAPHQL_HOST,
 });
