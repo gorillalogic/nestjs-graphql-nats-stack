7ed002a924124b06f3ab91696e21da6386baab64
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-05-17 04:27:40 -0600

    Add styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
