ff7079f618ed4ed41767a1b1639f71eba63f51df
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-05-22 11:09:08 -0600

    Add cloudfront config

--- terraform/certs.tf
+++ terraform/certs.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "main" {
   domain_name       = "${var.domain}"
   validation_method = "DNS"
+  subject_alternative_names = ["*.${var.domain}"]
 
   lifecycle {
     create_before_destroy = true
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -37,3 +37,62 @@ EOF
     aws_s3_bucket_public_access_block.main,
   ]
 }
+
+resource "aws_cloudfront_distribution" "app_s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.main.website_endpoint
+    origin_id = "S3-.${aws_s3_bucket.frontend.bucket}"
+
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+
+  aliases = [var.frontend_domain]
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-.${aws_s3_bucket.frontend.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.main.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+}
