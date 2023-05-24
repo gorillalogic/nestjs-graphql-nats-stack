bd67e9bd3054ce5d799bad1cd1bcc52d676fad7f
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2023-05-24 09:45:25 -0600

    Moved fargate tasks to private network

--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -150,8 +150,7 @@ resource "aws_ecs_service" "main" {
 
   network_configuration {
     security_groups = [aws_security_group.ecs_tasks.id]
-    subnets = aws_subnet.public.*.id
-    assign_public_ip = true
+    subnets = aws_subnet.private.*.id
   }
 
   load_balancer {
--- terraform/routes.tf
+++ terraform/routes.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "private" {
 resource "aws_route" "private" {
   route_table_id = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.main.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route_table_association" "private" {
